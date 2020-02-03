9ea444d81ab8aa92079665c9b9e945dac20e51dd
izumiito19 <izumiito19@gmail.com>
2020-02-03 22:58:55 +0900

    おみくじの実装

--- omikuji/app.js
+++ omikuji/app.js
@@ -4,29 +4,18 @@ var btn = document.getElementById("omikuji");
 
 function OnButtonClick() {
   alert("おみくじをひくよ");
-}
 
-/*
-(function() {
-  btn.addEventListener("click", function() {
-    var n = Math.random();
-    if (n < 0.3) {
-      this.textContent = "大吉"; // 30%
-    } else if (n < 0.45) {
-      this.textContent = "吉"; // 15%
-    } else if (n < 0.6) {
-      this.textContent = "中吉"; // 15%
-    } else if (n < 0.85) {
-      this.textContent = "小吉"; // 15%
-    } else {
-      this.textContent = "凶"; // 15%
-    }
-  });
-  btn.addEventListener("mousedown", function() {
-    this.className = "pushed";
-  });
-  btn.addEventListener("mouseup", function() {
-    this.className = "";
-  });
-})();
-  */
+  var label = document.getElementById("kekka");
+  var n = Math.random();
+  if (n < 0.3) {
+    label.innerText = "大吉"; // 30%
+  } else if (n < 0.45) {
+    label.innerText = "吉"; // 15%
+  } else if (n < 0.6) {
+    label.innerText = "中吉"; // 15%
+  } else if (n < 0.85) {
+    label.innerText = "小吉"; // 15%
+  } else {
+    label.innerText = "凶"; // 15%
+  }
+}
