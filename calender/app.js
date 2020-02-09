c7beae40367bd827d010f73a6fefcde06b24627f
izumiito19 <izumiito19@gmail.com>
2020-02-09 22:24:53 +0900

    土日と今日を色付け

--- calender/app.js
+++ calender/app.js
@@ -13,7 +13,7 @@ document.querySelector("#yearMonth").innerHTML = `${today.getFullYear()}/${(
 /* カレンダー */
 
 // tableタグ開始
-calendarHtml += '<table border="1">';
+calendarHtml += '<table border="1" id="calendarTable">';
 
 // 曜日作成
 calendarHtml += "<tr>";
@@ -22,18 +22,23 @@ calendarHtml += "</tr>";
 
 // 1日が何曜日か調べる
 const dayOfWeek = new Date(today.getFullYear(), today.getMonth(), 1).getDay();
-
 // 1日〜その週末までの日の配列を作成
 const firstDays = [...Array(7 - dayOfWeek).keys()].map(ary => ++ary);
-
 // 当月の日数を取得
-var lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+const lastDay = new Date(
+  today.getFullYear(),
+  today.getMonth() + 1,
+  0
+).getDate();
 
 //日付を描画
 function displayDays() {
   var isFirstTime = true;
   var isContinue = true;
   var nextDay = 0;
+
+  var nowRow = 1;
+  var todayRowCol = [];
 
   function f() {
     while (isContinue) {
@@ -57,50 +62,43 @@ function displayDays() {
           isContinue = false;
         }
       }
+      //今日の場所（row,col）取得
+      const nowCol = week.indexOf(today.getDate());
+      if (nowCol >= 0) {
+        todayRowCol = [nowRow, nowCol];
+      }
+
       //次週の始まりを退避
       nextDay = week[week.length - 1] + 1;
+      nowRow++;
 
       //HTML作成
       calendarHtml += "<tr>";
-      week.forEach(day => (calendarHtml += `<th>${day}</th>`));
+      week.forEach(day => (calendarHtml += `<td>${day}</td>`));
       calendarHtml += "</tr>";
     }
+    return todayRowCol;
   }
-  f();
+  return f();
 }
-displayDays();
-
-/*
-var isFirstTime = true;
-var nextDay = 0;
-for (let i = 1; i <= 7; i++) {
-  // 1週間分の配列を作成
-  var week = Array(7).fill("");
-
-  if (isFirstTime) {
-    //配列を途中から上書き
-    week.splice(dayOfWeek, 7 - dayOfWeek, ...firstDays);
-    isFirstTime = false;
-  } else {
-    console.log(nextDay);
-    week = [...Array(7).keys()].map(ary => ary + nextDay);
-  }
-  console.log(week);
-  nextDay = week[week.length - 1] + 1;
-
-  calendarHtml += "<tr>";
-  week.forEach(day => (calendarHtml += `<th>${day}</th>`));
-  calendarHtml += "</tr>";
-}
-*/
-//最初の週を描画
-//TODO描画処理は１つにまとめられそう
-
-// 残りの日数が7以上の場合は、7日描画
-
-// 残りの日数が7未満の場合は、残りを全て描画
+const todayRowCol = displayDays();
 
 // tableタグ終了
 calendarHtml += "</table>";
 // HTMLに反映
 document.querySelector("#calendar").innerHTML = calendarHtml;
+
+// セルの色付け
+var table = document.getElementById("calendarTable");
+for (var i = 0; i < table.rows.length; i++) {
+  // 土日
+  table.rows[i].cells[0].style.color = "Red";
+  table.rows[i].cells[6].style.color = "Blue";
+
+  for (var j = 0; j < table.rows[i].cells.length; j++) {
+    // 今日
+    if (i === todayRowCol[0] && j === todayRowCol[1]) {
+      table.rows[i].cells[j].style.backgroundColor = "pink";
+    }
+  }
+}
