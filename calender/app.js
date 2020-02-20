626b49f92b7aa12bffe1eab24e603f8c9f9d1319
izumiito19 <izumiito19@gmail.com>
2020-02-20 17:45:12 +0900

    リファクタリング

--- calender/app.js
+++ calender/app.js
@@ -1,36 +1,23 @@
 "use strict";
 
-function createDateData(year, month, day, dayOfWeek) {
-  return {
-    year: year,
-    month: month,
-    day: day,
-    dayOfWeek: dayOfWeek,
-    color:
-      today.getMonth() + 1 !== month
-        ? "gray"
-        : today.getMonth() + 1 === month && dayOfWeek === 0
-        ? "red"
-        : today.getMonth() + 1 === month && dayOfWeek === 6
-        ? "blue"
-        : ""
-  };
-}
-
-var calendarHtml = "";
 const oneWeek = ["日", "月", "火", "水", "木", "金", "土"];
 
+let datebuff = new Date("2020-2-25");
+const today = {
+  year: datebuff.getFullYear(),
+  month: datebuff.getMonth() + 1,
+  date: datebuff.getDate()
+};
+
 /* 対象年月 */
-let today = new Date();
-document.querySelector("#yearMonth").innerHTML = `${today.getFullYear()}/${(
-  "0" +
-  (today.getMonth() + 1)
+document.querySelector("#yearMonth").innerHTML = `${today.year}/${(
+  "0" + today.month
 ).slice(-2)}`;
 
 /* カレンダー */
 
 // tableタグ開始
-calendarHtml += '<table border="1" id="calendarTable">';
+let calendarHtml = '<table border="1" id="calendarTable">';
 
 // ヘッダ(曜日)作成
 calendarHtml += "<tr>";
@@ -38,27 +25,23 @@ oneWeek.forEach(youbi => (calendarHtml += `<th>${youbi}</th>`));
 calendarHtml += "</tr>";
 
 // 1日が何曜日か調べる
-const dayOfWeek = new Date(today.getFullYear(), today.getMonth(), 1).getDay();
+const dayOfWeek = new Date(`${today.year}/${today.month}/1`).getDay();
+
 // 1日〜その週末までの日の配列を作成
 const firstDays = [...Array(7 - dayOfWeek).keys()].map(ary => ++ary);
 // 月の日数を取得
-const lastDay = new Date(
-  today.getFullYear(),
-  today.getMonth() + 1,
-  0
-).getDate();
-
+const daysOfMonth = new Date(today.year, today.month, 0).getDate();
 //日付を描画
 //カレンダー表示用配列作成
 let dayArrays = [];
 
 // 当月作成
-for (var i = 0; i < lastDay; i++) {
-  dayArrays[i] = createDateData(
-    today.getFullYear(),
-    today.getMonth() + 1,
+for (let i = 0; i < daysOfMonth; i++) {
+  dayArrays[i] = createDayData(
+    today.year,
+    today.month,
     i + 1,
-    new Date(today.getFullYear(), today.getMonth(), i + 1).getDay()
+    new Date(`${today.year}/${today.month}/${i + 1}`).getDay()
   );
 }
 
@@ -66,16 +49,12 @@ for (var i = 0; i < lastDay; i++) {
 let tempMonth = [];
 
 // 日数を取得
-let lastmonthDays = new Date(
-  today.getFullYear(),
-  today.getMonth(),
-  0
-).getDate();
+let lastmonthDays = new Date(today.year, today.month - 1, 0).getDate();
 
-for (var i = 0; i < dayArrays[0].dayOfWeek; i++) {
-  tempMonth[i] = createDateData(
-    today.getFullYear(),
-    today.getMonth(),
+for (let i = 0; i < dayArrays[0].dayOfWeek; i++) {
+  tempMonth[i] = createDayData(
+    today.year,
+    today.month - 1,
     lastmonthDays - dayArrays[0].dayOfWeek + i,
     i
   );
@@ -88,9 +67,9 @@ tempMonth = [];
 var cnt = 6 - dayArrays[dayArrays.length - 1].dayOfWeek;
 
 for (var i = 0; i < cnt; i++) {
-  tempMonth[i] = createDateData(
-    today.getFullYear(),
-    today.getMonth() + 2,
+  tempMonth[i] = createDayData(
+    today.year,
+    today.month + 1,
     i + 1,
     dayArrays[dayArrays.length - 1].dayOfWeek + i + 1
   );
@@ -109,16 +88,16 @@ dayArrays.forEach(function(aDay) {
     calendarHtml2 += ` style = "color:${aDay.color}" `;
   }
 
-  if (aDay.month !== today.getMonth() + 1) {
+  if (aDay.month !== today.month) {
     calendarHtml2 += ` bgcolor = "lightgray" `;
   } else if (
-    today.getFullYear() === aDay.year &&
-    today.getMonth() + 1 === aDay.month &&
-    today.getDate() === aDay.day
+    today.year === aDay.year &&
+    today.month === aDay.month &&
+    today.date === aDay.date
   ) {
     calendarHtml2 += ` bgcolor = "pink" `;
   }
-  calendarHtml2 += `>${aDay.day}</td>`;
+  calendarHtml2 += `>${aDay.date}</td>`;
 
   if (aDay.dayOfWeek === 6) {
     calendarHtml2 += "</tr>";
@@ -140,3 +119,20 @@ document.querySelector("#calendar").innerHTML = calendarHtml;
       );
       console.log(holidays[0]["name"]); // 敬老の日
       */
+
+function createDayData(year, month, day, dayOfWeek) {
+  return {
+    year: year,
+    month: month,
+    date: day,
+    dayOfWeek: dayOfWeek,
+    color:
+      today.month !== month
+        ? "gray"
+        : today.month === month && dayOfWeek === 0
+        ? "red"
+        : today.month === month && dayOfWeek === 6
+        ? "blue"
+        : ""
+  };
+}
